abf3ecfdb272e958456c1a1715a8953c06c97efd
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2020-01-13 07:31:44 +0200

    modify land and potrait layout

--- app/src/main/java/com/example/trackit/MainActivity.java
+++ app/src/main/java/com/example/trackit/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.trackit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startTrip(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_start_trip,
+                Toast.LENGTH_SHORT);
+
+        toast.show();
     }
 }
