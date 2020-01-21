a39a7ff5f7563deb15d752217f071cbe66849c43
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2020-01-21 07:24:16 +0200

    ask for location permission

--- app/src/main/java/com/example/trackit/MainActivity.java
+++ app/src/main/java/com/example/trackit/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.trackit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final String TRACKING_LOCATION_KEY = "tracking_location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +25,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startTrip(View view) {
+
+        getLocation();
+
         Toast toast = Toast.makeText(this, R.string.toast_start_trip,
                 Toast.LENGTH_SHORT);
 
         toast.show();
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            Log.d(LOG_TAG, "getLocation: permissions granted");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                // If the permission is granted, get the location,
+                // otherwise, show a Toast
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    Toast.makeText(this,
+                            "Permission Denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
